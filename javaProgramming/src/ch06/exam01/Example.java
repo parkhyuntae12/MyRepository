6eb4245d5f46d73b2a3c1958c2a86fe6ec2d296d
Administrator <Administrator@COM-PC>
2016-08-23 17:20:55 +0900

    class

---
+++ javaProgramming/src/ch06/exam01/Example.java
@@ -0,0 +1,11 @@
+package ch06.exam01;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car myCar = new Car();
+		Tire myTyre = new Tire();
+	}
+
+}
---
+++ javaProgramming/src/ch06/exam02/StudentExample.java
@@ -0,0 +1,11 @@
+package ch06.exam02;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student();//생성자호출
+		Student s2 = new Student();
+	}
+
+}
