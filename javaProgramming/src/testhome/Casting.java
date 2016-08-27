5e95c4d3aa6e2cc0968834848fb1cdaf3203ea89
Administrator <Administrator@COM-PC>
2016-08-27 17:42:24 +0900

    dfedf22e2e

---
+++ javaProgramming/src/testhome/Casting.java
@@ -0,0 +1,19 @@
+package testhome;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intvalue = 10;
+		char charvalue ='A';
+		double doublevalue=5.7;
+		String strvalue="A";
+		
+		double var = (double) intvalue;
+		byte var1 = (byte) intvalue;
+		int var2 = (int)doublevalue;
+		char var3 = (char)strvalue;
+	}
+
+}
+zas
