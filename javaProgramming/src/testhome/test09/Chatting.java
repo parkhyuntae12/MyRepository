3b0d9b1878b28113c956d5e9d417b2c85ed6cb28
parkhyuntae <qkrgusxo1993@naver.com>
2016-09-27 10:56:20 +0900

    SpringProgramming 추가

--- javaProgramming/src/testhome/test09/Chatting.java
+++ javaProgramming/src/testhome/test09/Chatting.java
@@ -1,12 +1,19 @@
 package testhome.test09;
 
-/*public class Chatting {
+public class Chatting {
 	void startChat(String chatId){
-	String nickname=null;
-	nickname = chatId;
-	Chat chat = new Chat(){
-		
-	}
+//	String nickname=null;
+//	nickname = chatId;
+//	Chat chat = new Chat(){
+//	  @override
+//	  public void start(){
+//		  while(true){
+//			  String inputData = "æ»≥Á«œººø‰";
+//			  String message = "["+nickname+"]"+inputData;
+//			  sendMessage(message);
+//		  }
+//	  }
+//	};
+//	cha
 	}
 }	
-*/
