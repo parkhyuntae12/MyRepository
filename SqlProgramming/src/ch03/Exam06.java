658a7b2ebbd9dbdc62117405e61c47fbe2f18dfc
parkhyuntae12 <qkrgusxo1993@naver.com>
2016-10-18 22:26:21 +0900

    dddds3434

--- SqlProgramming/src/ch03/Exam06.java
+++ SqlProgramming/src/ch03/Exam06.java
@@ -18,10 +18,10 @@ public class Exam06 {
 		System.out.println(emp.getEmpno()+":"+emp.getEname()+":"+emp.getSal());
 		
 		
-		List<Department> result = getDepartment("개발");
-		for(Department dept: result){
-			System.out.println(dept.getDeptno()+":"+dept.getDname()+":"+dept.getLoc());
-		}
+//		List<Department> result = getDepartment("개발");
+//		for(Department dept: result){
+//			System.out.println(dept.getDeptno()+":"+dept.getDname()+":"+dept.getLoc());
+//		}
 	}
 
 	private static Employee getEmployee(int searchEmpno) throws ClassNotFoundException, SQLException {
@@ -46,43 +46,43 @@ public class Exam06 {
 		return null;
 	}
 	
-	private static List<Department> getDepartment(String searchDname) {
-		//select * from dept where dname like ? 
-		Scanner scanner = new Scanner(System.in);
-		while(true){
-			System.out.print("검색할 이름: ");
-			String name= scanner.nextLine();
-			List<Department> result = getList(name); 
-			for(Department dept:result){
-				System.out.println(dept.getDeptno()+":"+dept.getDname());
-			}
-			
-		}
-	}
-	public static List<Department> getList(String searchDname){
-		List<Department> list = new ArrayList<>();
-		Connection conn=null;
-		try{
-			Class.forName("oracle.jdbc.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "tester1","kosa12345");
-			String sql="select * from dept where dname like ?";
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, "%"+searchDname+"%");
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()){
-				Department dept = new Department();
-				dept.setDeptno(rs.getInt("deptno"));
-				dept.setDname(rs.getString("dname"));
-				dept.setLoc(rs.getString("loc"));
-				list.add(dept);
-			}
-			rs.close();
-			pstmt.close();
-			conn.close();
-		}catch(Exception e){
-			try{conn.close();} catch(SQLException e2){}
-			e.printStackTrace();
-		}
-		return list;
-	}
+//	private static List<Department> getDepartment(String searchDname) {
+//		//select * from dept where dname like ? 
+//		Scanner scanner = new Scanner(System.in);
+//		while(true){
+//			System.out.print("검색할 이름: ");
+//			String name= scanner.nextLine();
+//			List<Department> result = getList(name); 
+//			for(Department dept:result){
+//				System.out.println(dept.getDeptno()+":"+dept.getDname());
+//			}
+//			
+//		}
+//	}
+//	public static List<Department> getList(String searchDname){
+//		List<Department> list = new ArrayList<>();
+//		Connection conn=null;
+//		try{
+//			Class.forName("oracle.jdbc.OracleDriver");
+//			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "tester1","kosa12345");
+//			String sql="select * from dept where dname like ?";
+//			PreparedStatement pstmt = conn.prepareStatement(sql);
+//			pstmt.setString(1, "%"+searchDname+"%");
+//			ResultSet rs = pstmt.executeQuery();
+//			while(rs.next()){
+//				Department dept = new Department();
+//				dept.setDeptno(rs.getInt("deptno"));
+//				dept.setDname(rs.getString("dname"));
+//				dept.setLoc(rs.getString("loc"));
+//				list.add(dept);
+//			}
+//			rs.close();
+//			pstmt.close();
+//			conn.close();
+//		}catch(Exception e){
+//			try{conn.close();} catch(SQLException e2){}
+//			e.printStackTrace();
+//		}
+//		return list;
+	//}
 }
